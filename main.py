da4000be31d08702176d217d76dbf92bb45b1490
AmirHossein Ameli <amirahosseinameli@gmail.com>
2021-11-20 15:33:01 +0330

    done till 3 digits.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ one_digit_dict = {
     'نه' : 9
 }
 
-two_digit_dict = {
+two_digit_dict1 = {
     'ده': 10,
     'یازده': 11,
     'دوازده': 12,
@@ -25,7 +25,10 @@ two_digit_dict = {
     'شونزده': 16,
     'هفده': 17,
     'هجده': 18,
-    'نوزده': 19,
+    'نوزده': 19
+}
+
+two_digit_dict2 = {
     'بیست': 20,
     'سی': 30,
     'چهل': 40,
@@ -44,29 +47,40 @@ three_digits_dict = {
     'پانصد' : 500,
 }
 
+value_list = ['صد', 'هزار', 'میلیون', 'میلیارد']
+
 decimal_separator = ['\\', '.', ',', '٫']
 decimal_separator_pattern = "|".join(re.escape(separator) for separator in decimal_separator)
 
 def get_dict_pattern(input_dict):
     return "|".join(re.escape(key) for key in input_dict.keys())
 
+def remove_pattern_groups_matching(pattern):
+    return re.sub('\((?!\?)', '(?:', pattern)
+
+class Decimal():
+
+    def __init__(self):
+        self.digit_pattern = '(?:' + decimal_separator_pattern + ')\d*'
+        self.pattern = None #todo
+
+    def convert(self):
+        pass #todo
 
 class OneDigit():
 
     def __init__(self, include_decimal = True):
+        self.digit_pattern = '(0*\d{1})'
+        self.pattern1 = '(' + get_dict_pattern(one_digit_dict) + ')'
         if (include_decimal):
-            self.digit_pattern = '0*\d(?:' + decimal_separator_pattern + ')\d+'
-        else:
-            self.digit_pattern = '0*[\d]'
-        if (include_decimal):
-            self.pattern1 = '(' + get_dict_pattern(one_digit_dict) + ')\s*و\s*' #todo + 
-        else:
-            self.pattern1 = '(' + get_dict_pattern(one_digit_dict) + ')'
+            self.decimal = Decimal()
+            self.digit_pattern += '(?:' + self.decimal.digit_pattern + ')?'
+            self.pattern1 += '(' + get_dict_pattern(one_digit_dict) + ')(?:\s*و\s*(' + self.decimal.pattern + '))?' #todo
 
-    def convert(self, text):
+    def convert(self, text): #todo
         search = re.search(self.digit_pattern, text)
         if search:
-            return float(text)
+            return float(search.group(1))
         search = re.search(self.pattern1, text)
         if search:
             res = 0
@@ -75,3 +89,86 @@ class OneDigit():
         
         return None
 
+    def get_class_patterns(self):
+        class_pattern = self.digit_pattern + '|' + self.pattern1
+        return remove_pattern_groups_matching(class_pattern)
+
+
+class TwoDigit():
+
+    def __init__(self, include_decimal = True):
+        self.digit_pattern = '(0*\d{2})'
+        self.one_digit = OneDigit(include_decimal = include_decimal)
+        self.pattern1 = '(' + get_dict_pattern(two_digit_dict1) + ')'
+        self.pattern2 = '(' + get_dict_pattern(two_digit_dict2) + ')(?:\s*و\s*(' +  self.one_digit.get_class_patterns() + '))?'
+        self.pattern3 = '(' + self.one_digit.get_class_patterns() + ')'
+        if (include_decimal): #todo
+            self.decimal = Decimal()
+            self.digit_pattern += '(?:' + self.decimal.digit_pattern + ')?'
+            self.pattern1 += '(' + get_dict_pattern(one_digit_dict) + ')(?:\s*و\s*' + '(' + self.decimal.pattern + '))?'
+
+    def convert(self, text):
+        search = re.search(self.digit_pattern, text)
+        if search:
+            return float(search.group(1))
+        search = re.search(self.pattern1, text)
+        if search:
+            res = 0
+            res += two_digit_dict1[search.group(1)]
+            return res
+        search = re.search(self.pattern2, text)
+        if search:
+            res = 0
+            res += two_digit_dict2[search.group(1)]
+            if search.group(2) is not None:
+                res += self.one_digit.convert(search.group(2))
+            return res
+        search = re.search(self.pattern3, text)
+        if search:
+            res = self.one_digit.convert(search.group(1))
+            return res
+        return None
+
+    def get_class_patterns(self):
+        class_pattern = self.digit_pattern + '|' + self.pattern1 + '|' + self.pattern2 + '|' + self.pattern3 
+        return remove_pattern_groups_matching(class_pattern)
+
+
+class Hundred():
+
+    def __init__(self, include_decimal = True):
+        self.digit_pattern = '(0*\d{3})'
+        self.two_digit = TwoDigit(include_decimal = include_decimal)
+        hundred_part = '(?:(' + self.two_digit.get_class_patterns() + ')\s*' + value_list[0] + \
+                        '|(' + get_dict_pattern(three_digits_dict) + '))'
+        two_digit_part = '(' + self.two_digit.get_class_patterns() + ')'
+        self.pattern1 = hundred_part + '(?:\s*و\s*' + two_digit_part + ')?'
+        self.pattern2 = two_digit_part
+        if (include_decimal): #todo
+            self.decimal = Decimal()
+            self.digit_pattern += '(?:' + self.decimal.digit_pattern + ')?'
+            self.pattern1 += '(' + get_dict_pattern(one_digit_dict) + ')(?:\s*و\s*' + '(' + self.decimal.pattern + '))?'
+
+    def convert(self, text):
+        search = re.search(self.digit_pattern, text)
+        if search:
+            return float(search.group(1))
+        search = re.search(self.pattern1, text)
+        if search:
+            res = 0
+            if search.group(1) is not None:
+                res += 100 * self.two_digit.convert(search.group(1))
+            if search.group(2) is not None:
+                res += three_digits_dict[search.group(2)]
+            if search.group(3) is not None:
+                res += self.two_digit.convert(search.group(3))
+            return res
+        search = re.search(self.pattern2, text)
+        if search:
+            res = self.two_digit.convert(search.group(1))
+            return res
+        return None
+
+    def get_class_patterns(self):
+        class_pattern = self.digit_pattern + '|' + self.pattern1 + '|' + self.pattern2
+        return remove_pattern_groups_matching(class_pattern)
